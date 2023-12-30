47c302bbe1679521d1f43853f31caf63d5a2cf00
josh4500 <jibo.ajosh45@gmail.com>
2023-12-30 05:02:40 +0100

    Added controller to `SettingsListView`

--- lib/presentation/screens/settings/widgets/settings_list_view.dart
+++ lib/presentation/screens/settings/widgets/settings_list_view.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 
-class SettingsListView extends StatelessWidget {
+class SettingsListView extends StatefulWidget {
   final List<Widget> children;
 
   const SettingsListView({super.key, required this.children});
+
+  @override
+  State<SettingsListView> createState() => _SettingsListViewState();
+}
+
+class _SettingsListViewState extends State<SettingsListView> {
+  final scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +26,10 @@ class SettingsListView extends StatelessWidget {
       axisDirection: AxisDirection.up,
       color: Colors.grey,
       child: Scrollbar(
+        controller: scrollController,
         child: ListView(
-          children: children,
+          controller: scrollController,
+          children: widget.children,
         ),
       ),
     );
