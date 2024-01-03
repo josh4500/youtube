3f91172494d8f9f59deedbd5c6f1fd35bc6b8e6f
josh4500 <jibo.ajosh45@gmail.com>
2024-01-03 11:49:21 +0100

    Added usable models

--- lib/domain/models/account.dart
+++ lib/domain/models/account.dart
@@ -1,11 +1,15 @@
 class Account {
   final String id;
+  final String? avatar;
+  final bool? hasChannel;
   final String name;
   final String username;
   final String email;
 
   const Account({
     required this.id,
+    this.avatar,
+    this.hasChannel,
     required this.name,
     required this.username,
     required this.email,
