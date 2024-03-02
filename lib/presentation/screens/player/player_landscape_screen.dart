bdc7ea53c7d598599bdfda755430bad27bea4eff
josh4500 <jibo.ajosh45@gmail.com>
2024-03-02 19:45:20 +0100

    Fix animation values for Slide and Scale while swiping player in landscape mode

--- lib/presentation/screens/player/player_landscape_screen.dart
+++ lib/presentation/screens/player/player_landscape_screen.dart
@@ -1,3 +1,31 @@
+// Copyright (c) 2023, Ajibola Akinmosin (https://github.com/josh4500)
+
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//       this list of conditions and the following disclaimer in the documentation
+//       and/or other materials provided with the distribution.
+//     * Neither the name of {{ project }} nor the names of its contributors
+//       may be used to endorse or promote products derived from this software
+//       without specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -8,6 +36,7 @@ import 'package:youtube_clone/core/constants/constants.dart';
 import 'package:youtube_clone/presentation/widgets/custom_scroll_physics.dart';
 
 import '../../providers.dart';
+import '../../view_models/playback/player_sizing.dart';
 import 'widgets/player/player.dart';
 import 'widgets/player/player_components_wrapper.dart';
 import 'widgets/player/player_notifications.dart';
@@ -54,15 +83,11 @@ class _PlayerLandscapeScreenState extends ConsumerState<PlayerLandscapeScreen>
 
     _slideAnimation = Tween<Offset>(
       begin: const Offset(0, 0),
-      end: const Offset(0, .4),
+      end: const Offset(0, .05),
     ).animate(_viewController);
 
-    _scaleAnimation = ReverseAnimation(
-      CurvedAnimation(
-        parent: _viewController,
-        curve: const Interval(0, .4, curve: Curves.easeInCubic),
-        reverseCurve: const Interval(.4, 0, curve: Curves.easeOutCubic),
-      ),
+    _scaleAnimation = _viewController.drive(
+      Tween(begin: 1, end: 0.95),
     );
 
     Future(() async {
@@ -185,18 +210,18 @@ class _PlayerLandscapeScreenState extends ConsumerState<PlayerLandscapeScreen>
             child: childWidget!,
           );
         },
-        child: ProviderScope(
-          overrides: [
-            playerSizingProvider.overrideWithValue(
-              PlayerSizing(
-                minHeight: 1,
-                maxHeight: 1,
+        child: Hero(
+          tag: 'player',
+          child: ProviderScope(
+            overrides: [
+              playerSizingProvider.overrideWithValue(
+                PlayerSizing(
+                  minHeight: 1,
+                  maxHeight: 1,
+                ),
               ),
-            ),
-          ],
-          child: const Hero(
-            tag: 'player',
-            child: KeyedSubtree(
+            ],
+            child: const KeyedSubtree(
               child: PlayerView(),
             ),
           ),
