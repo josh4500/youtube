13239c2c25da2bb61ec3a8f47b679fd0f21c3049
josh4500 <jibo.ajosh45@gmail.com>
2024-06-05 00:43:21 +0100

    [Task] Added `Interceptor`

--- lib/infrastructure/worker/task/async_queue_lock.dart
+++ lib/infrastructure/worker/task/async_queue_lock.dart
@@ -5,13 +5,18 @@ import '../utils.dart';
 import 'async_task.dart';
 
 class AsyncTaskQueue<R, S> {
-  AsyncTaskQueue({this.onDone});
+  AsyncTaskQueue({
+    this.onDone,
+    this.interceptors = const <Interceptor>[],
+  });
+
   int _idCounter = 0;
   bool _isLock = false;
 
   bool isTypeVoid() => S == voidType();
 
   final FutureOr<void> Function(S value)? onDone;
+  final List<Interceptor> interceptors;
   final Queue<AsyncTask<R, S>> _queue = Queue<AsyncTask<R, S>>();
 
   void addEvent(R event, AsyncTaskOperation<R, S> operation) {
@@ -20,6 +25,10 @@ class AsyncTaskQueue<R, S> {
       input: event,
       operation: operation,
     );
+
+    for (final interceptor in interceptors) {
+      interceptor.onAdd(task);
+    }
 
     if (_isLock) {
       _queue.add(task);
@@ -32,6 +41,10 @@ class AsyncTaskQueue<R, S> {
   void _doTask(AsyncTask<R, S> task) {
     final Completer<S> completer = Completer<S>();
     completer.future.then((S value) async {
+      for (final interceptor in interceptors) {
+        interceptor.onComplete(task);
+      }
+
       if (!isTypeVoid() && onDone != null) {
         await onDone!(value);
       }
@@ -43,4 +56,16 @@ class AsyncTaskQueue<R, S> {
     });
     completer.complete(task.operation(task.input));
   }
+}
+
+abstract class Interceptor {
+  void onAdd(AsyncTask task);
+  void onComplete(AsyncTask task);
+}
+
+class CacheInterceptor extends Interceptor {
+  @override
+  void onAdd(AsyncTask task) {}
+  @override
+  void onComplete(AsyncTask task) {}
 }
