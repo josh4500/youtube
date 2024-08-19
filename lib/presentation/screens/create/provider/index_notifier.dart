8ba3d46170d163022861b9d54e06ce5a0aef86e9
josh4500 <jibo.ajosh45@gmail.com>
2024-08-19 11:47:19 +0100

    Refactor `PreExitCaptureOptionsSheet`

--- lib/presentation/screens/create/provider/index_notifier.dart
+++ lib/presentation/screens/create/provider/index_notifier.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_clone/presentation/models.dart';
 
+/// An index notifier use for tab or IndexStack.
+///
+/// Provide value through [ModelBinding] for use within [TabIndexListenerMixin]
 class IndexNotifier<T extends Enum> extends ValueNotifier<T> {
   IndexNotifier(super.value);
   int get currentIndex => value.index;
 }
 
+/// Listen to IndexNotifier for index changes
+///
+/// Use [onIndexChanged] within state Tab widget. [IndexNotifier] must be provided
+/// through [ModelBinding]
 mixin TabIndexListenerMixin<T extends StatefulWidget> on State<T> {
   IndexNotifier? _indexNotifier;
 
